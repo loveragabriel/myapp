a217cc656b6f61aee8964a60465c1ca195229be9
loveragabriel <loveragabriel20@gmail.com>
2022-06-04 16:14:09 -0300

    changes made on NavBar component

--- src/Components/NavComponent.js
+++ src/Components/NavComponent.js
@@ -5,36 +5,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 const NavComponent = () =>{
-    return(
-        <div>
-            <div className="nav-container">
-                <nav className="NavBar">
-                    <h1 className="web-title">
-                       Icon
-                    </h1>
-                    <ul>
-                        <li className="btn-about  ">
-                            <Link to="/aboutUs">
-                                About Us
-                            </Link>
-                        </li>
-                        <li className="btn-contact">
-                            <Link to="/Contact">
-                                Contacs
-                            </Link>
-                        </li>
-                    </ul>
-                    <div>
-                        <button>
-                            <Link to="/">
-                                Log Out
-                            </Link>
-                        </button>
-                    </div>            
-                </nav>
+        return(
+            <div>
+                <div className="nav-container">
+                    <nav className="NavBar">
+                       <div className="nav-icon">
+                       <i className="fa-solid fa-jug-detergent"></i>                    </div>
+                        <ul>
+                            <li className="btn-about  ">
+                                <Link to="/aboutUs">
+                                    About Us
+                                </Link>
+                            </li>
+                            <li className="btn-contact">
+                                <Link to="/Contact">
+                                    Contacs
+                                </Link>
+                            </li>
+                            <li className="btn-contact">
+                                <Link to="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="btn-contact">
+                                <Link to="/">
+                                    LogOut
+                                </Link>
+                            </li>
+                        </ul>
+                        <div>
+                        </div>            
+                    </nav>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 
 export default NavComponent; 
